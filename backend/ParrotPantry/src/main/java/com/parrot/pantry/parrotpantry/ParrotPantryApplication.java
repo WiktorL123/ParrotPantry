b76172f4f87d75d0c3a190928aefbb35fae36faa
wiktor <wiktorlem1@wp.pl>
2024-11-26 21:30:59 +0100

    zaicjowano projekt

---
+++ backend/ParrotPantry/src/main/java/com/parrot/pantry/parrotpantry/ParrotPantryApplication.java
@@ -0,0 +1,13 @@
+package com.parrot.pantry.parrotpantry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParrotPantryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParrotPantryApplication.class, args);
+    }
+
+}

f820cba76d48edb39380ae9e3c02a2c56582b034
s26987 <s26987@pjwstk.edu.pl>
2024-12-07 17:54:54 +0100

    crudy usera, dziala get, delete post jeszcze ma error 500

---
+++ backend/ParrotPantry/src/main/java/com/parrot/pantry/parrotpantry/user/User.java
@@ -0,0 +1,93 @@
+package com.parrot.pantry.parrotpantry.user;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "\"user\"")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private LocalDate created_at;
+    private boolean is_admin;
+
+    public User() {
+    }
+
+    public User(int id, String username,
+                String password, String email,
+                LocalDate created_at, boolean is_admin) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.created_at = created_at;
+        this.is_admin = is_admin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDate created_at) {
+        this.created_at = created_at;
+    }
+
+    public boolean isIs_admin() {
+        return is_admin;
+    }
+
+    public void setIs_admin(boolean is_admin) {
+        this.is_admin = is_admin;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", created_at=" + created_at +
+                ", is_admin=" + is_admin +
+                '}';
+    }
+}
---
+++ backend/ParrotPantry/src/main/java/com/parrot/pantry/parrotpantry/user/UserController.java
@@ -0,0 +1,51 @@
+package com.parrot.pantry.parrotpantry.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody User user) {
+        userService.addUser(user);
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody User updatedUser) {
+        User user = userService.updateUser(updatedUser);
+        if(user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable int id) {
+        boolean deleted = userService.deleteUserById(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build(); // 204 No Content
+        } else {
+            return ResponseEntity.notFound().build(); // 404 Not Found
+        }
+    }
+
+}

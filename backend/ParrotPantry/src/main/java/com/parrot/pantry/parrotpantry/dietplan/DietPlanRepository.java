88c3b03aeac28e9917a7c1d3176ee42fc17cd301
wiktorlemanski <wiktorlem1@wp.pl>
2025-01-17 11:27:33 +0100

    frontend i backend java zniknelo

--- backend/ParrotPantry/src/main/java/com/parrot/pantry/parrotpantry/dietplan/DietPlanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.parrot.pantry.parrotpantry.dietplan;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DietPlanRepository extends JpaRepository<DietPlan, Integer> {
-}
--- backend/ParrotPantry/src/main/java/com/parrot/pantry/parrotpantry/veterinarian/VeterinarianRepository.java
+++
@@ -1,8 +0,0 @@
-package com.parrot.pantry.parrotpantry.veterinarian;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VeterinarianRepository extends JpaRepository<Veterinarian, Integer> {
-}
